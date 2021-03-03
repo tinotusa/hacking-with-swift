b71a063de86174da585a6781b46aab32499432f7
Tino Tusaumwe <tusatino@gmail.com>
2021-03-03 19:54:08 +1000

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Learning how to make Apps with swift and SwiftUI
+Following tutorials from [Hacking with swift](hackingwithswift.com)
+# Started on 03/03/21
---
+++ WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Tino on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
