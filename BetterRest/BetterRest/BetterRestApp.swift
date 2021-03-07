f41b65d60149bda344505bf382b18049c7e8233d
Tino Tusaumwe <tusatino@gmail.com>
2021-03-07 11:12:32 +1000

    BetterRest first commit

---
+++ BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Tino on 7/3/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
