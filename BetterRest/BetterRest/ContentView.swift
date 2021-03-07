d2eb827d62ba8097ae35b999954e0b4308232803
Tino Tusaumwe <tusatino@gmail.com>
2021-03-07 11:40:09 +1000

    Add basic UI layout

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -8,9 +8,57 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var wakeUp = Date()
+    @State private var sleepAmount = 8.0
+    @State private var coffeeAmount = 1
+    
     var body: some View {
-        Text("Hello, world")
+        NavigationView {
+            VStack {
+                Text("When do you want to wake up?")
+                    .headline()
+                
+                DatePicker("Please enter a time", selection: $wakeUp, displayedComponents: .hourAndMinute)
+                    .labelsHidden()
+                
+                Text("Desired amount of sleep")
+                    .headline()
+                
+                Stepper(value: $sleepAmount, in: 6 ... 12, step: 0.25) {
+                    Text("\(sleepAmount, specifier: "%g") hours")
+                }
+                
+                Text("Daily coffee intake")
+                    .headline()
+                
+                Stepper(value: $coffeeAmount, in: 1 ... 20) {
+                    Text("\(coffeeAmount) \(pluralize("cup", count: coffeeAmount))")
+                }
+            }
             .padding()
+            .navigationBarTitle("BetterRest")
+        }
+    }
+    
+    func pluralize(_ text: String, count: Int) -> String {
+        if count == 1 {
+            return text
+        }
+        return text + "s"
+    }
+    
+}
+
+struct HeadLine: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.headline)
+    }
+}
+
+extension View {
+    func headline() -> some View {
+        self.modifier(HeadLine())
     }
 }
 
