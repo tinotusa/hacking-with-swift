1c235229fdb4674bddaa6104fa25e0a606178337
Tino Tusaumwe <tusatino@gmail.com>
2021-03-23 19:25:14 +1000

    Add Animations project

---
+++ Animations/Animations/AnimationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationsApp.swift
+//  Animations
+//
+//  Created by Tino on 18/3/21.
+//
+
+import SwiftUI
+
+@main
+struct AnimationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
