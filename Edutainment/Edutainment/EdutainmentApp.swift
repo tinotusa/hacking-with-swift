8099d4752b85461c179371373a8695a4b6761ac8
Tino Tusaumwe <tusatino@gmail.com>
2021-03-24 16:45:21 +1000

    Add Edutainment project

---
+++ Edutainment/Edutainment/EdutainmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EdutainmentApp.swift
+//  Edutainment
+//
+//  Created by Tino on 24/3/21.
+//
+
+import SwiftUI
+
+@main
+struct EdutainmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
