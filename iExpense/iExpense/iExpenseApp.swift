c879ffdeaef4a0e65d63d38b3b6c8bcf2822bbd0
Tino Tusaumwe <tusatino@gmail.com>
2021-03-24 23:21:58 +1000

    Add iExpense project

---
+++ iExpense/iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Tino on 24/3/21.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
