376782f3a6b3e6288b6e64a5b59ef38544b05a51
Tino Tusaumwe <tusatino@gmail.com>
2021-03-26 17:20:10 +1000

    Add Drawing project

---
+++ Drawing/Drawing/DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  DrawingApp.swift
+//  Drawing
+//
+//  Created by Tino on 26/3/21.
+//
+
+import SwiftUI
+
+@main
+struct DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
