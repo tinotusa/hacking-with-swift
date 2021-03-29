79b9bb58f847294409526a83a10810616bcfa1e1
Tino Tusaumwe <tusatino@gmail.com>
2021-03-29 22:22:00 +1000

    Add CupcakeCorner project

---
+++ CupcakeCorner/CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Tino on 29/3/21.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
