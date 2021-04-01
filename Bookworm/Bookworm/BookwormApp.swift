cbc60597b97cac29f5e2e72e7e618c2f00b236d6
Tino Tusaumwe <tusatino@gmail.com>
2021-04-01 15:02:23 +1000

    Add Bookworm project

---
+++ Bookworm/Bookworm/BookwormApp.swift
@@ -0,0 +1,20 @@
+//
+//  BookwormApp.swift
+//  Bookworm
+//
+//  Created by Tino on 1/4/21.
+//
+
+import SwiftUI
+
+@main
+struct BookwormApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
