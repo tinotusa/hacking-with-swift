4bf2bfb81fff5839056b19bd659db3d7f8f19526
Tino Tusaumwe <tusatino@gmail.com>
2021-04-04 19:17:44 +1000

    Added Instafilter project

---
+++ Instafilter/Instafilter/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instafilter
+//
+//  Created by Tino on 4/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {    
+    var body: some View {
+        Text("Hello, world")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Tino on 4/4/21.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
