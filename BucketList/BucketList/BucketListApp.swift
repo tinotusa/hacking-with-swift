6e882fc4a1606d4fd144c27e3c73d467e04e4b70
Tino Tusaumwe <tusatino@gmail.com>
2021-04-05 11:18:15 +1000

    Add BucketList project

---
+++ BucketList/BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Tino on 5/4/21.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
