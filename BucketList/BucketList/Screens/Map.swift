7c1d8dcf504f91544b3926dbc684e54e4702adeb
Tino Tusaumwe <tusatino@gmail.com>
2021-04-05 20:34:37 +1000

    Add some map functionality

---
+++ BucketList/BucketList/Screens/Map.swift
@@ -0,0 +1,20 @@
+//
+//  Map.swift
+//  BucketList
+//
+//  Created by Tino on 5/4/21.
+//
+
+import SwiftUI
+
+struct Map: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Map_Previews: PreviewProvider {
+    static var previews: some View {
+        Map()
+    }
+}
---
+++ BucketList/BucketList/Screens/MapView.swift
@@ -0,0 +1,91 @@
+//
+//  MapView.swift
+//  BucketList
+//
+//  Created by Tino on 5/4/21.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView {
+    @Binding var centerCoordinate: CLLocationCoordinate2D
+    @Binding var selectedPlace: MKPointAnnotation?
+    @Binding var showingPlaceDetails: Bool
+    var annotations: [MKPointAnnotation]
+}
+
+extension MapView: UIViewRepresentable {
+    func makeUIView(context: Context) -> MKMapView {
+        let mapView = MKMapView()
+        mapView.delegate = context.coordinator
+        return mapView
+    }
+    
+    func updateUIView(_ view: MKMapView, context: Context) {
+        if annotations.count != view.annotations.count {
+            view.removeAnnotations(view.annotations)
+            view.addAnnotations(annotations)
+            
+        }
+    }
+}
+
+extension MapView {
+    class Coordinator: NSObject, MKMapViewDelegate {
+        let parent: MapView
+        
+        init(_ parent: MapView) {
+            self.parent = parent
+        }
+        
+        func mapViewDidChangeVisibleRegion(_ mapView: MKMapView) {
+            parent.centerCoordinate = mapView.centerCoordinate
+        }
+        
+        func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+            let identifier = "Placemark"
+            var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: identifier)
+            
+            if annotationView == nil {
+                annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: identifier)
+                annotationView?.canShowCallout = true
+                annotationView?.rightCalloutAccessoryView = UIButton(type: .detailDisclosure)
+            } else {
+                annotationView?.annotation = annotation
+            }
+            
+            return annotationView
+        }
+        
+        func mapView(_ mapView: MKMapView, annotationView view: MKAnnotationView, calloutAccessoryControlTapped control: UIControl) {
+            guard let placemark = view.annotation as? MKPointAnnotation else { return }
+            parent.selectedPlace = placemark
+            parent.showingPlaceDetails = true
+        }
+    }
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+}
+
+extension MKPointAnnotation {
+    static var example: MKPointAnnotation {
+        let annotation = MKPointAnnotation()
+        annotation.title = "London"
+        annotation.subtitle = "Home to the 2012 Summer Olympics."
+        annotation.coordinate = CLLocationCoordinate2D(latitude: 51.5, longitude: -0.13)
+        return annotation
+    }
+}
+
+struct MapView_Previews: PreviewProvider {
+    static var previews: some View {
+        MapView(
+            centerCoordinate: .constant(MKPointAnnotation.example.coordinate),
+            selectedPlace: .constant(MKPointAnnotation.example),
+            showingPlaceDetails: .constant(true),
+            annotations: [MKPointAnnotation.example]
+        )
+    }
+}
