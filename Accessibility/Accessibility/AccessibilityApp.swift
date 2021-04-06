50737db0c2386097b20e095c7696e29dcf8379c4
Tino Tusaumwe <tusatino@gmail.com>
2021-04-06 15:45:29 +1000

    Add Accessibility project

---
+++ Accessibility/Accessibility/AccessibilityApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccessibilityApp.swift
+//  Accessibility
+//
+//  Created by Tino on 6/4/21.
+//
+
+import SwiftUI
+
+@main
+struct AccessibilityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
