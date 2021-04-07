f9837d8cb9fa4f958d668e992c3bc8236af7a9a4
Tino Tusaumwe <tusatino@gmail.com>
2021-04-07 15:16:06 +1000

    Add HotProspects project

---
+++ HotProspects/HotProspects/HotProspectsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HotProspectsApp.swift
+//  HotProspects
+//
+//  Created by Tino on 7/4/21.
+//
+
+import SwiftUI
+
+@main
+struct HotProspectsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
