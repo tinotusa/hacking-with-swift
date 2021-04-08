44e3a8845c5c2cd1f81a33a1f59fa7dbe0c60928
Tino Tusaumwe <tusatino@gmail.com>
2021-04-08 10:13:54 +1000

    Add Flashzilla project

---
+++ Flashzilla/Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Tino on 8/4/21.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
