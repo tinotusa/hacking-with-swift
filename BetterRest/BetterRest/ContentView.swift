43cc863f8ecb804f8c54ef3589d519c0f05b9cd2
Tino Tusaumwe <tusatino@gmail.com>
2021-04-19 09:53:11 +1000

    Change BetterRest UI, update README

--- BetterRest/BetterRest/ContentView.swift
+++ BetterRest/BetterRest/ContentView.swift
@@ -20,57 +20,106 @@ struct ContentView: View {
     
     @State private var wakeUp = defaultWakeTime
     @State private var sleepAmount = 8.0
-    @State private var coffeeAmount = 1
+    @State private var coffeeAmount = ""
+    @State private var isEditing = false
+    
+    var body: some View {
+        GeometryReader { proxy in
+            ZStack(alignment: .bottom) {
+                background
+                    .ignoresSafeArea()
+                Text("Picture by: Casey Horner")
+                    .whiteTitleFont()
+                    .opacity(0.1)
+                ScrollView(showsIndicators: false) {
+                    VStack {
+                        Spacer(minLength: proxy.size.height * 0.1)
+                        Group {
+                            Text("When do you want\n") +
+                            Text("to wake up?")
+                        }
+                        .whiteTitleFont()
+                        DatePicker("Wake up", selection: $wakeUp, displayedComponents: [.hourAndMinute])
+                            .labelsHidden()
+                        
+                        Group {
+                            Text("How much sleep\n") +
+                            Text("would you like to get?")
+                        }
+                        .whiteTitleFont()
+                        
+                        hoursStepper
+                            
+                        Text("Daily coffee intake:")
+                            .whiteTitleFont()
+                        cupPicker
+                        
+                        Text("Calculated sleep time:")
+                            .whiteTitleFont()
+                        Text("\(sleepTime)")
+                            .foregroundColor(.orange)
+                            .font(.title)
+                    }
+                    .multilineTextAlignment(.center)
+                }
+            }
+            .frame(width: proxy.size.width)
+        }
+    }
+}
+
+private extension ContentView {
+    var background: some View {
+        ZStack(alignment: .bottom) {
+            Image("nightsky")
+                .resizable()
+                .scaledToFill()
+            
+            Rectangle()
+                .fill(LinearGradient(
+                        gradient: Gradient(colors: [Color.black.opacity(0.7), Color.clear]),
+                        startPoint: .top,
+                        endPoint: .bottom)
+                )
+        }
+    }
+    
+    
+    var hoursStepper: some View {
+        ZStack {
+            Rectangle().fill(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            HStack {
+                Text("\(sleepAmount, specifier: "%g") hours")
+                    .foregroundColor(.gray)
+                Spacer()
+                Stepper("Sleep amount", value: $sleepAmount)
+                    .labelsHidden()
+            }
+            .padding(.horizontal)
+        }
+        .frame(width: 220, height: 45)
+    }
+
+    var cupPicker: some View {
+        ZStack {
+            Rectangle().fill(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: 10))
+            TextField("Cups", text: $coffeeAmount)
+                .keyboardType(.numberPad)
+        }
+        .frame(width: 220, height: 45)
+    }
     
     var sleepTime: String {
         calculateBedTime()
-    }
-    
-    var body: some View {
-        NavigationView {
-            Form {
-                Section(header: Text("When do you want to wake up?")) {
-                    DatePicker(
-                        "Please enter a time",
-                        selection: $wakeUp,
-                        displayedComponents: .hourAndMinute
-                    )
-                    .labelsHidden()
-                }
-                
-                Section(header: Text("Desired amount of sleep")) {
-                    Stepper(value: $sleepAmount, in: 6 ... 12, step: 0.25) {
-                        Text("\(sleepAmount, specifier: "%g") hours")
-                    }
-                    .accessibility(value: Text("\(sleepAmount, specifier: "%g") hours"))
-                }
-                
-                Section(header: Text("Daily coffee intake")) {
-                    Picker("Number of cups", selection: $coffeeAmount) {
-                        ForEach(1 ..< 21) { cup in
-                            Text("\(cup) \(pluralize("cup", count: cup))")
-                        }
-                    }
-                }
-                
-                Section(header: Text("Sleep time")) {
-                    HStack {
-                        Spacer()
-                        Text(sleepTime)
-                            .font(.largeTitle)
-                        Spacer()
-                    }
-                }
-            }
-            .navigationBarTitle("BetterRest")
-        }
     }
     
     func calculateBedTime() -> String {
         let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
         let hour = (components.hour ?? 0) * 60 * 60
         let minute = (components.minute ?? 0) * 60
-        
+        let coffeeAmount = Double(self.coffeeAmount) ?? 0
         do {
             let totalSeconds = Double(hour + minute)
             let prediction = try model.prediction(wake: totalSeconds, estimatedSleep: sleepAmount, coffee: Double(coffeeAmount))
@@ -85,17 +134,28 @@ struct ContentView: View {
         }
         return "Error, try again."
     }
-    
-    func pluralize(_ text: String, count: Int) -> String {
-        if count == 1 {
-            return text
-        }
-        return text + "s"
+}
+
+struct WhiteTitleFont: ViewModifier {
+    func body(content: Content) -> some View {
+        content
+            .font(.title)
+            .foregroundColor(.white)
+    }
+}
+
+extension View {
+    func whiteTitleFont() -> some View {
+        modifier(WhiteTitleFont())
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+//        ForEach(["iPhone 12", "iPod touch (7th generation)", "iPhone SE (2nd generation)"], id: \.self) { deviceName in
+//            ContentView()
+//                .previewDevice(PreviewDevice(rawValue: deviceName))
+//        }
     }
 }
--- README.md
+++ README.md
@@ -1,10 +1,13 @@
 # Learning how to make Apps with swift and SwiftUI
 Following tutorials from [Hacking with swift](hackingwithswift.com)
 
-### Completed tutorial projects
-|Project               |Images                                              |                                                              |                                                     |
-|----------------------|----------------------------------------------------|--------------------------------------------------------------|-----------------------------------------------------|
-| ✅ Project 1 WeSplit | ![WeSplit splash](projectImages/WeSplit/splash.png)|  ![WeSplit main screen](projectImages/WeSplit/mainScreen.png)|![WeSplit demo run](projectImages/WeSplit/appRun.mp4)|
+Started: 03/03/21
 
-### Started on 03/03/21
-### Completed on 12/04/21
+Completed: 12/04/21
+
+### Completed tutorial projects
+|Project               |                                                    |                                                              |                                                     |
+|----------------------|----------------------------------------------------|--------------------------------------------------------------|-----------------------------------------------------|
+| ✅ [Project 1 WeSplit](https://github.com/tinotusa/hacking-with-swift/tree/main/WeSplit) | ![WeSplit splash](projectImages/WeSplit/splash.png)|  ![WeSplit main screen](projectImages/WeSplit/mainScreen.png)|![WeSplit demo run](projectImages/WeSplit/demo.gif)|
+| ✅[Project 2 GuessTheFlag](https://github.com/tinotusa/hacking-with-swift/tree/main/GuessTheFlag) | ![GuessTheFlag splash](projectImages/GuessTheFlag/splash.png)| ![GuessTheFlag main menu](projectImages/GuessTheFlag/mainMenu.png) ![GuessTheFlag settings](projectImages/GuessTheFlag/settings.png) | ![GuessTheFlag settings](projectImages/GuessTheFlag/game.png) ![GuessTheFlag settings](projectImages/GuessTheFlag/gameOver.png)|
+| ✅ [Project 4 BetterRest](https://github.com/tinotusa/hacking-with-swift/tree/main/BetterRest) | ![BetterRest main screen](projectImages/BetterRest/main.png)|
