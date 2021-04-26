b3fa12f1bea1518cf8a25d45860a76c1b3142f6a
Tino Tusaumwe <tusatino@gmail.com>
2021-04-26 16:35:37 +1000

    Restarted project

--- Bookworm/Bookworm/ContentView.swift
+++ Bookworm/Bookworm/ContentView.swift
@@ -10,54 +10,13 @@ import CoreData
 
 
 struct ContentView: View {
-    @Environment(\.managedObjectContext) var moc
-    @FetchRequest(
-        entity: Book.entity(),
-        sortDescriptors: [
-            NSSortDescriptor(keyPath: \Book.title, ascending: true),
-            NSSortDescriptor(keyPath: \Book.author, ascending: true)
-        ]) var books: FetchedResults<Book>
-    
-    @State private var showingAddBookScreen = false
-    
     var body: some View {
-        NavigationView {
-            List {
-                ForEach(books) { book in
-                    NavigationLink(destination: BookDetail(book: book)) {
-                        BookRow(book: book)
-                    }
-                }
-                .onDelete(perform: deleteBooks)
-            }
-            .navigationBarTitle("Bookworm")
-            .navigationBarItems(
-                leading: EditButton(),
-                trailing: Button(action: { showingAddBookScreen = true }) {
-                    Image(systemName: "plus")
-                }
-            )
-            .sheet(isPresented: $showingAddBookScreen) {
-                AddBook()
-            }
-        }
-    }
-    
-    func deleteBooks(at offsets: IndexSet) {
-        for index in offsets {
-            let book = books[index]
-            moc.delete(book)
-        }
-        try? moc.save()
+        Text("hello world")
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         return ContentView()
-            .environment(
-                \.managedObjectContext,
-                PersistenceController.shared.container.viewContext
-            )
     }
 }
