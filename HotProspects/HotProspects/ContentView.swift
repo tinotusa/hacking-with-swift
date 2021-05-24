005377770253ee647535e5e08447a82c9a64bff5
Tino Tusaumwe <tusatino@gmail.com>
2021-05-24 18:42:26 +1000

    Restart HotProspects Project

---
+++ HotProspects/HotProspects/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  HotProspects
+//
+//  Created by Tino on 7/4/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("hello world")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
