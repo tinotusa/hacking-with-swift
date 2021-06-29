7041d9d77e6168c2c430350c0db5e156fa989334
Tino Tusaumwe <tusatino@gmail.com>
2021-06-29 21:19:24 +1000

    Restarted FlashZilla

---
+++ Flashzilla/Flashzilla/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Flashzilla
+//
+//  Created by Tino on 8/4/21.
+//
+
+import SwiftUI
+import CoreHaptics
+import AVKit
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
